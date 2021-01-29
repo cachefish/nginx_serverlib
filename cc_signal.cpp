f5fcceb56fd29dcbaa852a9ff8f46192ba74c516
cachefish <780207419@qq.com>
2021-01-29 13:47:45 +0800

    完善信号处理函数

--- cc_signal.cpp
+++ cc_signal.cpp
@@ -4,9 +4,12 @@
 #include<stdlib.h>
 #include<signal.h>
 #include<errno.h>
+#include<sys/wait.h>
+#include<sys/types.h>
 
 #include"c_macro.h"
 #include"c_func.h"
+#include"c_global.h"
 
 typedef struct{
     int signo;  //信号对应的数字编号
@@ -17,6 +20,8 @@ typedef struct{
 
 //声明一个信号处理函数
 static void cc_signal_handler(int signo, siginfo_t *siginfo, void *ucontext);
+static void ngx_process_get_status(void);   
+
 
 cc_signal_t signals[] = {
     {SIGHUP,"SIGHUP",cc_signal_handler},                        //终端断开信号
@@ -62,5 +67,104 @@ int cc_init_signals()
 //信号处理函数
 static void cc_signal_handler(int signo, siginfo_t *siginfo, void *ucontext)
 {
-    printf("来信号了\n");
+    //printf("来信号了\n");
+    cc_signal_t *sig;   //自定义结构
+    char *action;  
+
+    for(sig = signals;sig->signo != 0; sig++)   //遍历信号数组
+    {
+        if(sig->signo == signo)
+        {
+            break;
+        }
+    }
+
+    action = (char*)"";
+    
+    if(cc_process == CC_PROCESS_MASTER)
+    {
+        //master进程
+        switch (signo)
+        {
+        case SIGCHLD:
+            cc_reap = 1;    //标记子进程
+            break;
+        
+        default:
+            break;
+        }
+    }
+    else if(cc_process == CC_PROCESS_WORKER)    //worker进程
+    {
+        //后续
+    }
+    else 
+    {
+        //do nothing
+    }
+
+    //记录日志信息
+    if(siginfo && siginfo->si_pid)
+    {
+        cc_log_error_core(CC_LOG_NOTICE,0,"signal %d (%s) received from %P%s", signo, sig->signame, siginfo->si_pid, action);
+    }
+    else{
+                cc_log_error_core(CC_LOG_NOTICE,0,"signal %d (%s) received %s",signo, sig->signame, action);//没有发送该信号的进程id，所以不显示发送该信号的进程i
+    }
+
+    if(signo == SIGCHLD)    
+    {
+        cc_process_get_status();
+    }
+    return;
+}
+
+static void cc_process_get_status(void)
+{
+    pid_t pid;
+    int status;
+    int err;
+    int one  = 0;
+
+    for(;;){
+
+        pid = waitpid(-1,&status,WNOHANG);      //会立刻返回而不是等待子进程的状态发生变化
+
+        if(pid == 0)
+        {
+            return;
+        }
+
+        if(pid == -1)
+        {
+            err = errno;
+            if(err == EINTR)
+            {
+                continue;
+            }
+
+            if(err == ECHILD && one)
+            {
+                return;
+            }
+            if(err==ECHILD)
+            {
+                cc_log_error_core(CC_LOG_INFO,err,"waitpid() failed!");
+                return;
+            }
+            cc_log_error_core(CC_LOG_ALERT,err,"waitpid() failed!");
+            return;
+        }
+
+        one = 1;
+        if(WTERMSIG(status))    //获取使子进程终止的信号  测试作用通过信号来决定那个子线程被退出。  若成功返回被终止的子进程的信号值
+        {
+            cc_log_error_core(CC_LOG_ALERT,0,"pid = %P exited on signal %d!",pid,WTERMSIG(status));
+        }
+        else
+        {
+            cc_log_error_core(CC_LOG_NOTICE,0,"pid = %P exited with code %d!",pid,WEXITSTATUS(status));  //提取子进程的返回值
+        }
+    }
+    return;
 }
