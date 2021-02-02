ccba93bdd64b60561c44445488ad2644c48a09db
cachefish <780207419@qq.com>
2021-02-02 16:14:16 +0800

    update

--- c_printf.cpp
+++ c_printf.cpp
@@ -27,6 +27,19 @@ u_char *cc_slprintf(u_char *buf, u_char *last, const char *fmt, ...)
     return p;
 }
 
+u_char * cc_snprintf(u_char *buf, size_t max, const char *fmt, ...)   //类printf()格式化函数，比较安全，max指明了缓冲区结束位置
+{
+    u_char   *p;
+    va_list   args;
+
+    va_start(args, fmt);
+    p = cc_vslprintf(buf, buf + max, fmt, args);
+    va_end(args);
+    return p;
+}
+
+
+
 //----------------------------------------------------------------------------------------------------------------------
 //对于 nginx 自定义的数据结构进行标准格式化输出,就像 printf,vprintf 一样，我们顺道学习写这类函数到底内部是怎么实现的
 //例如，给进来一个 "abc = %d",13   ,最终buf里得到的应该是   abc=13 这种结果
