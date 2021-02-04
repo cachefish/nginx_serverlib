76d86318478f287e0a04df5d65039672f85dbef6
cachefish <780207419@qq.com>
2021-02-04 16:44:25 +0800

    分配内存函数

---
+++ c_memory.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#include"c_memory.h"
+
+
+CMemory *CMemory::m_instance = NULL;
+
+//分配内存
+//memCount：分配的字节大小
+//ifmemset：是否要把分配的内存初始化为0；
+void *CMemory::AllocMemory(int memCount,bool ifmemset)
+{	    
+	void *tmpData = (void *)new char[memCount]; 
+    if(ifmemset) //要求内存清0
+    {
+	    memset(tmpData,0,memCount);
+    }
+	return tmpData;
+}
+
+//内存释放函数
+void CMemory::FreeMemory(void *point)
+{		
+	
+    delete [] ((char *)point);
+}
---
+++ c_memory.h
@@ -0,0 +1,46 @@
+#ifndef __C_MEMORY_H__
+#define __C_MEMORY_H__
+#include<stddef.h>
+//分配内存
+
+class CMemory
+{
+    private:
+        CMemory(){}
+    public:
+        ~CMemory(){};
+    private:
+        static CMemory *m_instance;
+    public:
+        static CMemory* GetInstance()
+        {
+            if(m_instance == NULL)
+            {
+                if(m_instance == NULL){
+                     m_instance = new CMemory();
+                     static CCrecovery c1;
+                }
+               
+            }
+            return m_instance;
+        }
+        class CCrecovery{
+                public:
+                    ~CCrecovery()
+                    {
+                        if(CMemory::m_instance){
+                            delete CMemory::m_instance;
+                            CMemory::m_instance = NULL;
+                        }
+                    }
+        };
+
+    public:
+        void *AllocMemory(int memCount,bool ifmemset);
+        void FreeMemory(void *point);
+        
+};
+
+
+
+#endif
