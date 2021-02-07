a22afff0b633f5a8cefd36c76cb81ae2a30a608e
cachefish <780207419@qq.com>
2021-02-07 13:24:29 +0800

    update

--- c_process_cycle.cpp
+++ c_process_cycle.cpp
@@ -1,32 +1,35 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<string.h>
-#include<signal.h>
-#include<errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>   //信号相关头文件 
+#include <errno.h>    //errno
+#include <unistd.h>
 
-#include"c_func.h"
-#include"c_macro.h"
-#include"c_conf.h"
+#include "c_func.h"
+#include "c_macro.h"
+#include "c_conf.h"
 
+//函数声明
 static void cc_start_worker_processes(int threadnums);
 static int cc_spawn_process(int threadnums,const char *pprocname);
 static void cc_worker_process_cycle(int inum,const char *pprocname);
 static void cc_worker_process_init(int inum);
 
-//标题
-static u_char master_process[] = "master process";
+//变量声明
+static u_char  master_process[] = "master process";
 
-
-//创建worker子进程
+//描述：创建worker子进程
 void cc_master_process_cycle()
-{
-    sigset_t set;   //信号集
+{    
+    sigset_t set;        //信号集
 
-    sigemptyset(&set);
+    sigemptyset(&set);   //清空信号集
 
-    sigaddset(&set,SIGCHLD);    //子进程状态改变
-     sigaddset(&set, SIGALRM);     //定时器超时
+    //下列这些信号在执行本函数期间不希望收到【考虑到官方nginx中有这些信号，老师就都搬过来了】（保护不希望由信号中断的代码临界区）
+    //建议fork()子进程时学习这种写法，防止信号的干扰；
+    sigaddset(&set, SIGCHLD);     //子进程状态改变
+    sigaddset(&set, SIGALRM);     //定时器超时
     sigaddset(&set, SIGIO);       //异步I/O
     sigaddset(&set, SIGINT);      //终端中断符
     sigaddset(&set, SIGHUP);      //连接断开
@@ -35,52 +38,62 @@ void cc_master_process_cycle()
     sigaddset(&set, SIGWINCH);    //终端窗口大小改变
     sigaddset(&set, SIGTERM);     //终止
     sigaddset(&set, SIGQUIT);     //终端退出符
-
-    if(sigprocmask(SIG_BLOCK,&set,NULL) == -1){
-        cc_log_error_core(CC_LOG_ALERT,errno,"cc_master_process_cycle()中的sigprocmask()失败");
+    //.........可以根据开发的实际需要往其中添加其他要屏蔽的信号......
+    
+    //设置，此时无法接受的信号；阻塞期间，你发过来的上述信号，多个会被合并为一个，暂存着，等你放开信号屏蔽后才能收到这些信号。。。
+    //sigprocmask()在第三章第五节详细讲解过
+    if (sigprocmask(SIG_BLOCK, &set, NULL) == -1) //第一个参数用了SIG_BLOCK表明设置 进程 新的信号屏蔽字 为 “当前信号屏蔽字 和 第二个参数指向的信号集的并集
+    {        
+        cc_log_error_core(CC_LOG_ALERT,errno,"cc_master_process_cycle()中sigprocmask()失败!");
     }
+    //即便sigprocmask失败，程序流程 也继续往下走
 
-    //设置主进程标题
+    //首先我设置主进程标题---------begin
     size_t size;
-    int i;
-    size = sizeof(master_process);
-    size += g_argvneedmem;
-    if(size < 1000)
+    int    i;
+    size = sizeof(master_process);  //注意我这里用的是sizeof，所以字符串末尾的\0是被计算进来了的
+    size += g_argvneedmem;          //argv参数长度加进来    
+    if(size < 1000) //长度小于这个，我才设置标题
     {
         char title[1000] = {0};
-        strcpy(title,(const char *)master_process);
-        strcat(title," ");
-        for(i=0;i<g_os_argc;i++){
+        strcpy(title,(const char *)master_process); //"master process"
+        strcat(title," ");  //跟一个空格分开一些，清晰    //"master process "
+        for (i = 0; i < g_os_argc; i++)         //"master process ./nginx"
+        {
             strcat(title,g_os_argv[i]);
-        }
-        cc_setproctitle(title);
-
-        cc_log_error_core(CC_LOG_NOTICE,0,"%s %P[master进程]启动并开始运行......!",title,cc_pid);
-    }
+        }//end for
+        cc_setproctitle(title); //设置标题
+        cc_log_error_core(CC_LOG_NOTICE,0,"%s %P 【master进程】启动并开始运行......!",title,cc_pid); //设置标题时顺便记录下来进程名，进程id等信息到日志
+    }    
+    //首先我设置主进程标题---------end
+        
     //从配置文件中读取要创建的worker进程数量
-    CConfig *p_config = CConfig::GetInstance();
-    int workprocess = p_config->GetIntDefault("WorkerProcesses",1);
-    cc_start_worker_processes(workprocess);
+    CConfig *p_config = CConfig::GetInstance(); //单例类
+    int workprocess = p_config->GetIntDefault("WorkerProcesses",1); //从配置文件中得到要创建的worker进程数量
+    cc_start_worker_processes(workprocess);  //这里要创建worker子进程
 
-    sigemptyset(&set);//不在屏蔽任何信号。
-
-     for ( ;; ) 
+    //创建子进程后，父进程的执行流程会返回到这里，子进程不会走进来    
+    sigemptyset(&set); //信号屏蔽字为空，表示不屏蔽任何信号
+    
+    for ( ;; ) 
     {
 
     //    usleep(100000);
-       //cc_log_error_core(0,0,"haha--这是父进程，pid为%P",cc_pid);
+        //cc_log_error_core(0,0,"haha--这是父进程，pid为%P",cc_pid);
 
         //a)根据给定的参数设置新的mask 并 阻塞当前进程【因为是个空集，所以不阻塞任何信号】
         //b)此时，一旦收到信号，便恢复原先的信号屏蔽【我们原来的mask在上边设置的，阻塞了多达10个信号，从而保证我下边的执行流程不会再次被其他信号截断】
         //c)调用该信号对应的信号处理函数
         //d)信号处理函数返回后，sigsuspend返回，使程序流程继续往下走
+        //printf("for进来了！\n"); //发现，如果print不加\n，无法及时显示到屏幕上，是行缓存问题，以往没注意；可参考https://blog.csdn.net/qq_26093511/article/details/53255970
 
         sigsuspend(&set); //阻塞在这里，等待一个信号，此时进程是挂起的，不占用cpu时间，只有收到信号才会被唤醒（返回）；
                          //此时master进程完全靠信号驱动干活    
 
 //        printf("执行到sigsuspend()下边来了\n");
         
-///        printf("master进程休息1秒\n");      
+        //printf("master进程休息1秒\n");      
+        //cc_log_stderr(0,"haha--这是父进程，pid为%P",cc_pid); 
         sleep(1); //休息1秒        
         //以后扩充.......
 
@@ -111,7 +124,7 @@ static int cc_spawn_process(int inum,const char *pprocname)
     switch (pid)  //pid判断父子进程，分支处理
     {  
     case -1: //产生子进程失败
-        cc_log_error_core(CC_LOG_ALERT,errno,"ngx_spawn_process()fork()产生子进程num=%d,procname=\"%s\"失败!",inum,pprocname);
+        cc_log_error_core(CC_LOG_ALERT,errno,"cc_spawn_process()fork()产生子进程num=%d,procname=\"%s\"失败!",inum,pprocname);
         return -1;
 
     case 0:  //子进程分支
@@ -120,7 +133,7 @@ static int cc_spawn_process(int inum,const char *pprocname)
         cc_worker_process_cycle(inum,pprocname);    //我希望所有worker子进程，在这个函数里不断循环着不出来，也就是说，子进程流程不往下边走;
         break;
 
-    default: //这个应该是父进程分支，直接break;，流程往switch之后走        
+    default: //这个应该是父进程分支，直接break;，流程往switch之后走            
         break;
     }//end switch
 
@@ -130,42 +143,55 @@ static int cc_spawn_process(int inum,const char *pprocname)
 }
 
 //描述：worker子进程的功能函数，每个woker子进程，就在这里循环着了（无限循环【处理网络事件和定时器事件以对外提供web服务】）
-//     子进程分叉才会走到之类
+//     子进程分叉才会走到这里
 //inum：进程编号【0开始】
 static void cc_worker_process_cycle(int inum,const char *pprocname) 
 {
+    //设置一下变量
+    cc_process = CC_PROCESS_WORKER;  //设置进程的类型，是worker进程
 
-    cc_process = CC_PROCESS_WORKER;     //设置进程的类型
-    //重新为子进程设置进程名，不能与父进程重复
+    //重新为子进程设置进程名，不要与父进程重复------
     cc_worker_process_init(inum);
     cc_setproctitle(pprocname); //设置标题   
-    cc_log_error_core(CC_LOG_NOTICE,0,"%s %P [worker进程]启动并开始运行......!",pprocname,cc_pid);
-    
+    cc_log_error_core(CC_LOG_NOTICE,0,"%s %P 【worker进程】启动并开始运行......!",pprocname,cc_pid); //设置标题时顺便记录下来进程名，进程id等信息到日志
+
+
+    //测试代码，测试线程池的关闭
+    //sleep(5); //休息5秒        
+    //g_threadpool.StopAll(); //测试Create()后立即释放的效果
+
+    //暂时先放个死循环，我们在这个循环里一直不出来
+    //setvbuf(stdout,NULL,_IONBF,0); //这个函数. 直接将printf缓冲区禁止， printf就直接输出了。
     for(;;)
     {
 
         //先sleep一下 以后扩充.......
         //printf("worker进程休息1秒");       
         //fflush(stdout); //刷新标准输出缓冲区，把输出缓冲区里的东西打印到标准输出设备上，则printf里的东西会立即输出；
-        sleep(1); //休息1秒       
+        //sleep(1); //休息1秒       
         //usleep(100000);
         //cc_log_error_core(0,0,"good--这是子进程，编号为%d,pid为%P！",inum,cc_pid);
         //printf("1212");
         //if(inum == 1)
         //{
-            //ngx_log_stderr(0,"good--这是子进程，编号为%d,pid为%P",inum,ngx_pid); 
-            //printf("good--这是子进程，编号为%d,pid为%d\r\n",inum,ngx_pid);
-            //ngx_log_error_core(0,0,"good--这是子进程，编号为%d",inum,ngx_pid);
+            //cc_log_stderr(0,"good--这是子进程，编号为%d,pid为%P",inum,cc_pid); 
+            //printf("good--这是子进程，编号为%d,pid为%d\r\n",inum,cc_pid);
+            //cc_log_error_core(0,0,"good--这是子进程，编号为%d",inum,cc_pid);
             //printf("我的测试哈inum=%d",inum++);
             //fflush(stdout);
         //}
             
-        //ngx_log_stderr(0,"good--这是子进程，pid为%P",ngx_pid); 
-        //ngx_log_error_core(0,0,"good--这是子进程，编号为%d,pid为%P",inum,ngx_pid);
-        cc_process_events_and_timers();
+        //cc_log_stderr(0,"good--这是子进程，编号为%d,pid为%P",inum,cc_pid); 
+        //cc_log_error_core(0,0,"good--这是子进程，编号为%d,pid为%P",inum,cc_pid);
+
+        cc_process_events_and_timers(); //处理网络事件和定时器事件
+
+
     } //end for(;;)
 
-    g_threadpool.StopAll();
+    //如果从这个循环跳出来，考虑在这里停止线程池；
+    g_threadpool.StopAll(); 
+
     return;
 }
 
@@ -177,21 +203,25 @@ static void cc_worker_process_init(int inum)
     sigemptyset(&set);  //清空信号集
     if (sigprocmask(SIG_SETMASK, &set, NULL) == -1)  //原来是屏蔽那10个信号【防止fork()期间收到信号导致混乱】，现在不再屏蔽任何信号【接收任何信号】
     {
-        cc_log_error_core(CC_LOG_ALERT,errno,"ngx_worker_process_init()中sigprocmask()失败!");
+        cc_log_error_core(CC_LOG_ALERT,errno,"cc_worker_process_init()中sigprocmask()失败!");
     }
 
+    //线程池代码，率先创建，至少要比和socket相关的内容优先
     CConfig *p_config = CConfig::GetInstance();
-    int tmpthreadnums = p_config->GetIntDefault("ProcMsgRecvWorkThreadCount",5);
-    if(g_threadpool.Create(tmpthreadnums)==false)
+    int tmpthreadnums = p_config->GetIntDefault("ProcMsgRecvWorkThreadCount",5); //处理接收到的消息的线程池中线程数量
+    if(g_threadpool.Create(tmpthreadnums) == false)  //创建线程池中线程
     {
+        //内存没释放，但是简单粗暴退出；
         exit(-2);
     }
-    sleep(1);
-    g_socket.cc_epoll_init();
+    sleep(1); //再休息1秒；
+    
+    //如下这些代码参照官方nginx里的cc_event_process_init()函数中的代码
+    g_socket.cc_epoll_init();           //初始化epoll相关内容，同时 往监听socket上增加监听事件，从而开始让监听端口履行其职责
+    //g_socket.cc_epoll_listenportstart();//往监听socket上增加监听事件，从而开始让监听端口履行其职责【如果不加这行，虽然端口能连上，但不会触发cc_epoll_process_events()里边的epoll_wait()往下走】
+    
+    
     //....将来再扩充代码
     //....
     return;
 }
-
-
-
