eca2401949385b04063adeb798a56ba524dfc40d
cachefish <780207419@qq.com>
2021-02-07 17:19:18 +0800

    处理逻辑和通讯的子类

---
+++ c_slogic.h
@@ -0,0 +1,24 @@
+#ifndef __C_SLOGIC_H__
+#define __C_SLOGIC_H__
+
+#include <sys/socket.h>
+#include "c_socket.h"
+
+//处理逻辑和通讯的子类
+class CLogicSocket : public CSocket   //继承自父类CScoekt
+{
+public:
+	CLogicSocket();                                                         //构造函数
+	virtual ~CLogicSocket();                                                //释放函数
+	virtual bool Initialize();                                              //初始化函数
+
+public:
+	//各种业务逻辑相关函数都在之类
+	bool _HandleRegister(lpcc_connection_t pConn,LPSTRUC_MSG_HEADER pMsgHeader,char *pPkgBody,unsigned short iBodyLength);
+	bool _HandleLogIn(lpcc_connection_t pConn,LPSTRUC_MSG_HEADER pMsgHeader,char *pPkgBody,unsigned short iBodyLength);
+
+public:
+	virtual void threadRecvProcFunc(char *pMsgBuf);
+};
+
+#endif
