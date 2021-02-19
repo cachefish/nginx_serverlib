b98ee1b28f0b75b0c3a3b46334061a502bfb9e84
cachefish <780207419@qq.com>
2021-02-19 13:55:57 +0800

    每十秒显示信息

--- c_event.cpp
+++ c_event.cpp
@@ -13,7 +13,9 @@
 //处理网络事件和定时器事件
 void cc_process_events_and_timers()
 {
-    g_socket.cc_epoll_process_events(-1); //-1表示卡着等待吧
+    g_socket.cc_epoll_process_events(-1); //-1表示卡着等待
+    
+    g_socket.printInfo();
 
     //...再完善
 }
