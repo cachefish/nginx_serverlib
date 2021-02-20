4824b4bfd24c1d66eb31a99cd945c54bc7b83672
cachefish <780207419@qq.com>
2021-02-20 14:19:32 +0800

    设置进程标题

--- c_setproctitle.cpp
+++ c_setproctitle.cpp
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <unistd.h>  //env
 #include <string.h>
-
 #include "c_global.h"
 
 //设置可执行程序标题相关函数：分配内存，并且把环境变量拷贝到新内存中来
@@ -26,17 +25,15 @@ void cc_init_setproctitle()
 //设置可执行程序标题
 void cc_setproctitle(const char *title)
 {
-    
     //(1)计算新标题长度
     size_t ititlelen = strlen(title); 
-
     //(2)计算总的原始的argv那块内存的总长度【包括各种参数】    
     size_t esy = g_argvneedmem + g_envneedmem; //argv和environ内存总和
     if( esy <= ititlelen)
     {
         return;
     }
-    //(3)设置后续的命令行参数为空，表示只有argv[]中只有一个元素了，这是好习惯；防止后续argv被滥用，因为很多判断是用argv[] == NULL来做结束标记判断的;
+    //(3)设置后续的命令行参数为空
     g_os_argv[1] = NULL;  
 
     //(4)把标题弄进来，原来的命令行参数都会被覆盖掉
