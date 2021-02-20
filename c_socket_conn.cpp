e8b5504e443b67185c12b21f0882dd687051bf3d
cachefish <780207419@qq.com>
2021-02-20 14:29:01 +0800

    连接池函数

--- c_socket_conn.cpp
+++ c_socket_conn.cpp
@@ -118,7 +118,6 @@ lpcc_connection_t CSocket::cc_get_connection(int isock)
         p_Conn->fd = isock;
         return p_Conn;
     }
-
     //没有空闲的连接，则需要重新建立连接
     CMemory *p_memory = CMemory::GetInstance();
     lpcc_connection_t p_Conn = (lpcc_connection_t)p_memory;
@@ -130,38 +129,6 @@ lpcc_connection_t CSocket::cc_get_connection(int isock)
     ++m_total_connection_n;
     p_Conn->fd = isock;
     return p_Conn;
-
-    /*
-    lpcc_connection_t c = m_pfree_connections;  //空闲连接链表头
-
-    if(c == NULL){
-        cc_log_stderr(0,"CSocket::cc_get_connection()中空闲链表为空");
-        return NULL;
-    }
-
-    m_pfree_connections =  c->next;
-    m_free_connection_n--;
-
-    //把c指向的对象中有用的东西搞出来保存成变量
-    uintptr_t   instance = c->instance;
-    uint64_t    iCurrsequence = c->iCurrsequence;
-    //...
-
-
-    memset(c,0,sizeof(cc_connection_t));
-    c->fd = isock;   
-    c->curStat = _PKG_HD_INIT;                                              //收包状态处于 初始状态，准备接收数据包头
-    c->precvbuf = c->dataHeadInfo;                                      //收包我要先收到这里来，因为我要先收包头，所以收数据的buff直接就是dataHeadInfo
-    c->irecvlen = sizeof(COMM_PKG_HEADER);                //指定收数据的长度
-    c->ifnewrecvMem = false;
-    c->pnewMemPointer = NULL;
-    //...
-
-    c->instance = !instance;//分配内存时候，连接池里每个连接对象这个变量给的值都为1，所以这里取反应该是0【有效】；
-    c->iCurrsequence = iCurrsequence;
-    ++c->iCurrsequence; //每次取用该值都增加1
-    */
-    //return c;
 }
 
 //归还参数c所代表的连接到到连接池中
@@ -173,25 +140,10 @@ void CSocket::cc_free_connection(lpcc_connection_t pConn)
 
     m_freeconnectionList.push_back(pConn);
     ++m_free_connection_n;
-    /*
-    if(c->ifnewrecvMem == true){
-        CMemory::GetInstance()->FreeMemory(c->pnewMemPointer);
-        c->pnewMemPointer = NULL;
-        c->ifnewrecvMem        = false;
-    }
-
-    c->next = m_pfree_connections; //回收的节点指向原来串起来的空闲链的链头
-
-    ++c->iCurrsequence; //回收后，该值就增加1
-
-    m_pfree_connections = c;        //修改 原来的链头使链头指向新节点
-    ++m_free_connection_n;          //空闲连接+1
-    return;*/
     return;
 }
 
-//有些连接，我们不希望马上释放,将隔一段时间才释放的连接先放到一个队列中来
-
+//有些连接，我们不希望马上释放,将隔一段时间才释放的连接先放到一个队列中来  延迟回收
 void CSocket::inRecyConnectQueue(lpcc_connection_t pConn)
 {
     std::list<lpcc_connection_t>::iterator pos;
@@ -241,7 +193,6 @@ void* CSocket::ServerRecyConnectionThread(void *threadData)
 lblRRTD:
             pos = pSocketobj->m_recyconnectionList.begin();
             posend = pSocketobj->m_recyconnectionList.end();
-
             for(;pos!=posend;++pos){
                 p_Conn = (*pos);
                 if((p_Conn->inRecyTime + pSocketobj->m_RecyConnectionWaitTime)>currtime && (g_stopEvent == 0)){
@@ -250,16 +201,11 @@ lblRRTD:
                 //只要到达释放时间,iThrowsendCount就=0
                 if(p_Conn->iThrowsendCount > 0){
                         cc_log_stderr(0,"CSocket::ServerRecyConnectionThread()中到释放时间却发现p_Conn.iThrowsendCount!=0,不该发生");
-
                 }
-
-
                 --pSocketobj->m_total_recyconnection_n;
                 pSocketobj->m_recyconnectionList.erase(pos);
-
                 pSocketobj->cc_free_connection(p_Conn);
                 goto lblRRTD;
-
             }
             err = pthread_mutex_unlock(&pSocketobj->m_recyconnqueueMutex);
             if(err != 0 ) cc_log_stderr(err,"CSocekt::ServerRecyConnectionThread()pthread_mutex_unlock()失败，返回的错误码为%d!",err);
@@ -269,8 +215,7 @@ lblRRTD:
             if(pSocketobj->m_total_recyconnection_n>0)
             {
                 err = pthread_mutex_lock(&pSocketobj->m_recyconnqueueMutex);
-                if(err != 0) cc_log_stderr(err,"CSocket::ServerRecyConnectionThread()中pthread_mutex_lock2()失败，返回的错误码为%d!",err);
-            
+                if(err != 0) cc_log_stderr(err,"CSocket::ServerRecyConnectionThread()中pthread_mutex_lock2()失败，返回的错误码为%d!",err);   
         lblRRTD2:
                 pos = pSocketobj->m_recyconnectionList.begin();
                 posend = pSocketobj->m_recyconnectionList.end();
@@ -289,7 +234,6 @@ lblRRTD:
     }
     return (void*)0;
 }
-
 
 //用户连入，我们accept4()时，得到的socket在处理中产生失败，则资源用这个函数释放
 void CSocket::cc_close_connection(lpcc_connection_t pConn)
